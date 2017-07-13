78a4b26e57047daa09a9eccfdbe53edc139a8d0a
Rayhaneh Banyassady <r.b.assady@gmail.com>
2017-07-13 02:41:16 +0000

    Add looping exercise

---
+++ chorus-while.js
@@ -0,0 +1,17 @@
+var chorus = "Let's dance!";
+var repeat = 0;
+while (repeat < 10) {
+  if (repeat === 5) {
+    console.log("*change key*");
+  }
+  console.log(chorus);
+  repeat++;
+}
+console.log("Until the sun comes up!");
+console.log("\n");
+
+var chorus = "Let's dance!";
+for (var repeat = 0; repeat < 10; repeat++) {
+  console.log(chorus);
+}
+console.log("Until the sun comes up!");
